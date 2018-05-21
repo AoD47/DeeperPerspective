f23c95e82f7ea053d269f79309d0d4f669b19a33
Andre Kroukamp <andrekroukamp140791@gmail.com>
2018-05-21 22:10:50 +0200

    Startup Add Files

---
+++ DeeperPerspective/Models/ArtPiece.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeeperPerspective.Models
+{
+    public class ArtPiece
+    {
+        public string ID { get; set; }
+        public string FilePath { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public DateTime DateCreated { get; set; }
+        public double Price { get; set; }
+        public bool InStock { get; set; }
+        public bool CurrentOwner { get; set; }
+    }
+}
